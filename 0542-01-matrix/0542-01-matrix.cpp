d421eed4748a45b379a897a882305bf47bb104dd
PahuldeepSinghSodhi <sodhipahul24@gmail.com>
2026-07-11 14:37:37 +0530

    Time: 18 ms (59.63%), Space: 38.8 MB (38.18%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,57 +4,40 @@ public:
 
         int n = mat.size();
         int m = mat[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<vector<int>> dist(n,vector<int>(m,0));
+        queue <pair<pair<int,int>,int>> q;
 
-        queue<pair<int, int>> q;
-
-        vector<vector<int>> dist(n, vector<int>(m, 0));
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-
-                if (mat[i][j] == 0) {
-                    q.push({i, j});
+        for(int i = 0 ; i < n ; i++){
+            for( int j = 0 ; j < m ; j++){
+                if(mat[i][j] == 0){
+                    q.push({{i,j}, 0} );
                     vis[i][j] = 1;
                 }
-            }
-        }
-
-        int delRow[] = {-1, 0, 1, 0};
-        int delCol[] = {0, 1, 0, -1};
-
-        int steps = 0;
-
-        while (!q.empty()) {
-
-            int size = q.size();
-
-            for (int i = 0; i < size; i++) {
-
-                int row = q.front().first;
-                int col = q.front().second;
-                q.pop();
-
-                dist[row][col] = steps;
-
-                for (int j = 0; j < 4; j++) {
-
-                    int nrow = row + delRow[j];
-                    int ncol = col + delCol[j];
-
-                    if (nrow >= 0 && nrow < n &&
-                        ncol >= 0 && ncol < m &&
-                        !vis[nrow][ncol]) {
-
-                        vis[nrow][ncol] = 1;
-                        q.push({nrow, ncol});
-                    }
+                else{
+                    vis[i][j] = 0;
                 }
             }
-
-            steps++;
         }
 
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
+        while(!q.empty()){
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+            dist[row][col] = steps;
+            q.pop();
+            
+            for(int i = 0 ; i < 4 ; i++){
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+                if(nrow >= 0 && nrow < n && ncol >=0 && ncol < m && vis[nrow][ncol] == 0){
+                    vis[nrow][ncol] = 1;
+                    q.push({{nrow,ncol},steps+1});
+                }
+            }
+        }
         return dist;
     }
 };
