2990fe9d51963383e81feed287e3fb6a0d40a992
PahuldeepSinghSodhi <sodhipahul24@gmail.com>
2026-07-13 14:03:27 +0530

    Time: 2 ms (79.27%), Space: 19.5 MB (48.55%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,50 +1,44 @@
 class Solution {
+private:
+    bool dfsCheck(int node, vector<vector<int>>& adj, vector<int>& vis, vector<int>& pathVis) {
+
+        vis[node] = 1;
+        pathVis[node] = 1;
+
+        for (auto it : adj[node]) {
+
+            if (!vis[it]) {
+                if (dfsCheck(it, adj, vis, pathVis))
+                    return true;
+            }
+            else if (pathVis[it]) {
+                return true;
+            }
+        }
+
+        pathVis[node] = 0;
+        return false;
+    }
+
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
 
         vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses, 0);
 
-        // Create directed graph
-        for(auto it : prerequisites) {
-            int course = it[0];
-            int prerequisite = it[1];
-
-            adj[prerequisite].push_back(course);
-            indegree[course]++;
+        for (auto &it : prerequisites) {
+            adj[it[1]].push_back(it[0]);
         }
 
-        queue<int> q;
+        vector<int> vis(numCourses, 0);
+        vector<int> pathVis(numCourses, 0);
 
-        // Push courses having no prerequisite
-        for(int i = 0; i < numCourses; i++) {
-            if(indegree[i] == 0) {
-                q.push(i);
+        for (int i = 0; i < numCourses; i++) {
+            if (!vis[i]) {
+                if (dfsCheck(i, adj, vis, pathVis))
+                    return false;
             }
         }
 
-        int cnt = 0;
-
-        while(!q.empty()) {
-
-            int node = q.front();
-            q.pop();
-
-            cnt++;
-
-            for(auto adjnode : adj[node]) {
-
-                indegree[adjnode]--;
-
-                if(indegree[adjnode] == 0) {
-                    q.push(adjnode);
-                }
-            }
-        }
-
-        if(cnt == numCourses)
-            return true;
-
-        return false;
+        return true;
     }
 };
