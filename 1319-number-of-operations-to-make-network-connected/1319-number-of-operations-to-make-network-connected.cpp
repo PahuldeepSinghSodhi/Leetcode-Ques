cb42c1c3b336b966bfdaa167e359996360f89b0c
PahuldeepSinghSodhi <sodhipahul24@gmail.com>
2026-07-28 19:23:59 +0530

    Time: 64 ms (5.18%), Space: 75.7 MB (5.01%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,91 +1,50 @@
-class DisjointSet {
-public:
-    vector<int> parent, size, rank;
-
-    DisjointSet(int n) {
-        parent.resize(n);
-        size.resize(n, 1);
-        rank.resize(n, 0);
-
-        for (int i = 0; i < n; i++)
-            parent[i] = i;
-    }
-
-    int findparent(int node) {
-        if (node == parent[node])
-            return node;
-
-        return parent[node] = findparent(parent[node]);
-    }
-
-    bool find(int u, int v) {
-        return findparent(u) == findparent(v);
-    }
-
-    void unionbyrank(int u, int v) {
-        int pu = findparent(u);
-        int pv = findparent(v);
-
-        if (pu == pv)
-            return;
-
-        if (rank[pu] < rank[pv]) {
-            parent[pu] = pv;
-        }
-        else if (rank[pv] < rank[pu]) {
-            parent[pv] = pu;
-        }
-        else {
-            parent[pv] = pu;
-            rank[pu]++;
-        }
-    }
-
-    void unionbysize(int u, int v) {
-        int pu = findparent(u);
-        int pv = findparent(v);
-
-        if (pu == pv)
-            return;
-
-        if (size[pu] < size[pv]) {
-            parent[pu] = pv;
-            size[pv] += size[pu];
-        }
-        else {
-            parent[pv] = pu;
-            size[pu] += size[pv];
-        }
-    }
-};
-
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
 
-        DisjointSet ds(n);
-        int CntExtras = 0;
+        if (connections.size() < n - 1)
+            return -1;
 
-        for(auto it : connections){
+        vector<int> adj[n];
+
+        for (auto it : connections) {
             int u = it[0];
             int v = it[1];
 
-            if(ds.find(u,v)){
-                CntExtras++;
-            }
-            else ds.unionbysize(u,v);
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
 
-        int CntC = 0;
-        int ans;
-        for(int i = 0; i < n ; i++){
-            if(ds.parent[i] == i){
-                CntC++;
+        vector<int> vis(n, 0);
+
+        int components = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (!vis[i]) {
+
+                components++;
+
+                queue<int> q;
+                q.push(i);
+                vis[i] = 1;
+
+                while (!q.empty()) {
+
+                    int node = q.front();
+                    q.pop();
+
+                    for (auto it : adj[node]) {
+
+                        if (!vis[it]) {
+                            vis[it] = 1;
+                            q.push(it);
+                        }
+                    }
+                }
             }
         }
-        ans = CntC - 1;
-        if(CntExtras >= ans) return ans;
-        return -1;
-        
+
+        return components - 1;
     }
 };
