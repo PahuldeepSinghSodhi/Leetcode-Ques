f676464c5fec3ae07c1729efee74469578627739
PahuldeepSinghSodhi <sodhipahul24@gmail.com>
2026-08-11 18:57:25 +0530

    Time: 0 ms (100%), Space: 10.6 MB (47.25%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,16 +4,16 @@ public:
 
         int n  = nums.size();
         int prev1 = nums[0],prev2 = 0;
+        vector<int> dp(n+1,-1);
+        dp[0] = nums[0];
 
         for(int i = 1 ; i < n ; i ++){
 
-            int take = nums[i]; if(i > 1) take += prev2;
-            int nottake = 0 + prev1;
-            int curr = max(take,nottake);
-            prev2 = prev1;
-            prev1 = curr;
+            int take = nums[i]; if(i > 1) take += dp[i-2];
+            int nottake = 0 + dp[i-1];
+            dp[i] = max(take,nottake);
         }
 
-        return prev1;
+        return dp[n-1];
     }
 };
