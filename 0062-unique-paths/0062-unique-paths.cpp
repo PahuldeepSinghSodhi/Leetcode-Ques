ac1854466f77b3ec17d40201258f43b42ab39e4e
PahuldeepSinghSodhi <sodhipahul24@gmail.com>
2026-08-13 12:05:52 +0530

    Time: 2 ms (20.96%), Space: 9.3 MB (55.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -4,12 +4,13 @@ public:
         vector<vector<int>> dp(m,vector<int>(n,-1));
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
+                int up=0,left=0;
                 if(i == 0 && j == 0) {dp[0][0] = 1;}
-                else{ int up=0,left=0;
+                else {
                 if(i > 0) up = dp[i-1][j];
                 if(j > 0) left = dp[i][j-1];
                 dp[i][j] = up+left;
-                }
+            }
             }
         }
         return dp[m-1][n-1];
